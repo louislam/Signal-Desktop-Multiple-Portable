cbbdb371f7f14d495cf8568db29b3b8b37520e0e
LouisLam <louislam@users.noreply.github.com>
2021-04-01 17:10:23 +0800

    add ability to load the profile from the current dir

--- app/user_config.js
+++ app/user_config.js
@@ -1,6 +1,6 @@
 // Copyright 2017-2020 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
-
+const fs = require('fs');
 const path = require('path');
 
 const { app } = require('electron');
@@ -8,6 +8,26 @@ const { app } = require('electron');
 const { start } = require('./base_config');
 const config = require('./config');
 
+const dir = path.resolve('.');
+console.log(`original appData: ${app.getPath('appData')}`);
+console.log(`original userData: ${app.getPath('userData')}`);
+
+const profilesPath = path.join(dir, 'profiles');
+const profilePath = path.join(profilesPath, 'profile');
+console.log(`New Path: ${profilePath}`);
+
+if (!fs.existsSync(profilesPath)){
+  fs.mkdirSync(profilesPath);
+}
+
+if (!fs.existsSync(profilePath)){
+  fs.mkdirSync(profilePath);
+}
+
+app.setPath('userData', profilePath);
+console.log(`updated userData: ${app.getPath('userData')}`);
+
+/*
 // Use separate data directory for development
 if (config.has('storageProfile')) {
   const userData = path.join(
@@ -17,6 +37,7 @@ if (config.has('storageProfile')) {
 
   app.setPath('userData', userData);
 }
+*/
 
 console.log(`userData: ${app.getPath('userData')}`);
 
